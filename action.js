f378ed825ad3303f2fb67d2cc0c83bda439bd81b
Raj-StepSecurity <rajkumar@stepsecurity.io>
2026-03-06 12:07:38 +0530

    feat: onoboarding action-surefire-report
    added subscription check
    
    ci updated
    
    build updated

---
+++ action.js
@@ -0,0 +1,149 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const { Octokit } = require("@octokit/rest");
+const { retry } = require("@octokit/plugin-retry");
+const RetryingOctokit = Octokit.plugin(retry);
+const { parseTestReports } = require('./utils.js');
+const axios =  require('axios');
+
+async function validateSubscription() {
+    const repoPrivate = github.context?.payload?.repository?.private;
+    const upstream = 'dessant/lock-threads';
+    const action = process.env.GITHUB_ACTION_REPOSITORY;
+    const docsUrl = 'https://docs.stepsecurity.io/actions/stepsecurity-maintained-actions';
+
+    core.info('');
+    core.info('\u001b[1;36mStepSecurity Maintained Action\u001b[0m');
+    core.info(`Secure drop-in replacement for ${upstream}`);
+    if (repoPrivate === false) core.info('\u001b[32m\u2713 Free for public repositories\u001b[0m');
+    core.info(`\u001b[36mLearn more:\u001b[0m ${docsUrl}`);
+    core.info('');
+  
+    if (repoPrivate === false) return;
+    const serverUrl = process.env.GITHUB_SERVER_URL || 'https://github.com';
+    const body = { action: action || '' };
+    if (serverUrl !== 'https://github.com') body.ghes_server = serverUrl;
+
+    try {
+        await axios.post(
+            `https://agent.api.stepsecurity.io/v1/github/${process.env.GITHUB_REPOSITORY}/actions/maintained-actions-subscription`,
+            body, { timeout: 3000 }
+        );
+    } catch (error) {
+        if (axios.isAxiosError(error) && error.response?.status === 403) {
+            core.error(`\u001b[1;31mThis action requires a StepSecurity subscription for private repositories.\u001b[0m`);
+            core.error(`\u001b[31mLearn how to enable a subscription: ${docsUrl}\u001b[0m`);
+            process.exit(1);
+        }
+        core.info('Timeout or API not reachable. Continuing to next step.');
+    }
+}
+
+const action = async () => {
+    await validateSubscription();
+    const reportPaths = core.getInput('report_paths').split(',').join('\n');
+    core.info(`Going to parse results form ${reportPaths}`);
+    const githubToken = core.getInput('github_token');
+    const createCheck = (core.getInput('create_check') || 'true') === 'true';
+    const name = core.getInput('check_name');
+    const commit = core.getInput('commit');
+    const failOnFailedTests = core.getInput('fail_on_test_failures') === 'true';
+    const failIfNoTests = core.getInput('fail_if_no_tests') === 'true';
+    const ignoreFlakyTests = core.getInput('ignore_flaky_tests') === 'true';
+    const skipPublishing = core.getInput('skip_publishing') === 'true';
+    const isFilenameInStackTrace = core.getInput('file_name_in_stack_trace') === 'true';
+    const githubBaseUrl = core.getInput('github_base_url');
+
+    let { count, skipped, annotations } = await parseTestReports(reportPaths, isFilenameInStackTrace, ignoreFlakyTests);
+    const foundResults = count > 0 || skipped > 0;
+    const conclusion =
+        (foundResults && annotations.length === 0) || (!foundResults && !failIfNoTests)
+            ? 'success'
+            : 'failure';
+
+    function buildRetryingOctokitClient() {
+        const baseRequest = { auth: githubToken, request: { retries: 3 } };
+
+        if (githubBaseUrl){
+            baseRequest.baseUrl = githubBaseUrl;
+        }
+
+        return new RetryingOctokit(baseRequest)
+    }
+
+    if (!skipPublishing) {
+        const title = foundResults
+            ? `${count} tests run, ${skipped} skipped, ${annotations.length} failed.`
+            : 'No test results found!';
+        core.info(`Result: ${title}`);
+
+        const pullRequest = github.context.payload.pull_request;
+        const link = (pullRequest && pullRequest.html_url) || github.context.ref;
+        const status = 'completed';
+        const head_sha = commit || (pullRequest && pullRequest.head.sha) || github.context.sha;
+
+        const octokit = buildRetryingOctokitClient();
+        if (createCheck) {
+            core.info(`Posting status '${status}' with conclusion '${conclusion}' to ${link} (sha: ${head_sha})`);
+            const createCheckRequest = {
+                ...github.context.repo,
+                name,
+                head_sha,
+                status,
+                conclusion,
+                output: {
+                    title,
+                    summary: '',
+                    annotations: annotations.slice(0, 50)
+                }
+            };
+
+            core.debug(JSON.stringify(createCheckRequest, null, 2));
+
+            await octokit.rest.checks.create(createCheckRequest);
+        } else {
+            const { data: {check_runs: check_runs} } = await octokit.rest.checks.listForRef({
+                ...github.context.repo,
+                check_name: name,
+                ref: head_sha,
+                status: 'in_progress'
+            })
+            core.debug(JSON.stringify(check_runs, null, 2));
+            if (check_runs.length === 0) {
+                core.setFailed(`Did not find any in progress '${name}' check for sha ${head_sha}`);
+                return;
+            }
+            if (check_runs.length !== 1) {
+                core.setFailed(`Found multiple in progress '${name}' checks for sha ${head_sha}`);
+                return;
+            }
+            const check_run = check_runs[0];
+            core.info(`Patching '${name}' check for ${link} (sha: ${head_sha})`);
+            const updateCheckRequest = {
+                ...github.context.repo,
+                check_run_id: check_run.id,
+                output: {
+                    title: check_run.output.title || title,
+                    summary: check_run.output.summary || '',
+                    annotations: annotations.slice(0, 50)
+                }
+            };
+
+            core.debug(JSON.stringify(updateCheckRequest, null, 2));
+
+            await octokit.rest.checks.update(updateCheckRequest);
+        }
+    } else {
+        core.info('Not publishing test result due to skip_publishing=true');
+    }
+
+    // make conclusion consumable by downstream actions
+    core.setOutput('conclusion', conclusion);
+
+    // optionally fail the action if tests fail
+    if (failOnFailedTests && conclusion !== 'success') {
+        core.setFailed(`There were ${annotations.length} failed tests`);
+    }
+};
+
+module.exports = action;
---
+++ utils.js
@@ -0,0 +1,158 @@
+const glob = require('@actions/glob');
+const core = require('@actions/core');
+const fs = require('fs');
+const parser = require('xml-js');
+
+const resolveFileAndLine = (file, classname, output, isFilenameInOutput) => {
+    // extract filename from classname and remove suffix
+    let filename;
+    let filenameWithPackage;
+    if (isFilenameInOutput) {
+        filename = output.split(':')[0].trim();
+        filenameWithPackage = filename;
+    } else {
+        filename = file ? file : classname.split('.').slice(-1)[0].split('(')[0];
+        filenameWithPackage = classname.replace(/\./g, '/');
+    }
+    const escapedFilename = filename.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+    const matches = output.match(new RegExp(`${escapedFilename}.*?:\\d+`, 'g'));
+    if (!matches) return {filename: filename, filenameWithPackage: filenameWithPackage, line: 1};
+
+    const [lastItem] = matches.slice(-1);
+    const [, line] = lastItem.split(':');
+    core.debug(`Resolved file ${filenameWithPackage} with name ${filename} and line ${line}`);
+
+    return {filename, filenameWithPackage, line: parseInt(line)};
+};
+
+const resolvePath = async filenameWithPackage => {
+    core.debug(`Resolving path for ${filenameWithPackage}`);
+    const globber = await glob.create([`**/${filenameWithPackage}.*`, `**/${filenameWithPackage}`].join('\n'), {followSymbolicLinks: false});
+    const results = await globber.glob();
+    core.debug(`Matched files: ${results}`);
+    const searchPath = globber.getSearchPaths()[0];
+
+    let path = '';
+    if (results.length) {
+        // skip various temp folders
+        const found = results.find(r => !r.includes('__pycache__') && !r.endsWith('.class'));
+        if (found) path = found.slice(searchPath.length + 1);
+        else path = filenameWithPackage;
+    } else {
+        path = filenameWithPackage;
+    }
+    core.debug(`Resolved path: ${path}`);
+
+    // canonicalize to make windows paths use forward slashes
+    const canonicalPath = path.replace(/\\/g, '/');
+    core.debug(`Canonical path: ${canonicalPath}`);
+
+    return canonicalPath;
+};
+
+function getTestsuites(report) {
+    if (report.testsuite) {
+        return [report.testsuite];
+    }
+    if (!report.testsuites || !report.testsuites.testsuite) {
+        return [];
+    }
+    if (Array.isArray(report.testsuites.testsuite)) {
+        return report.testsuites.testsuite;
+    }
+    return [report.testsuites.testsuite];
+}
+
+async function parseFile(file, isFilenameInStackTrace, ignoreFlakyTests) {
+    core.debug(`Parsing file ${file}`);
+    let count = 0;
+    let skipped = 0;
+    let annotations = [];
+
+    const data = await fs.promises.readFile(file);
+
+    const report = JSON.parse(parser.xml2json(data, {compact: true}));
+    core.debug(`parsed report: ${JSON.stringify(report)}`);
+
+    const testsuites = getTestsuites(report);
+    core.debug(`test suites: ${JSON.stringify(testsuites)}`);
+
+    for (const testsuite of testsuites) {
+        const testcases = Array.isArray(testsuite.testcase)
+            ? testsuite.testcase
+            : testsuite.testcase
+                ? [testsuite.testcase]
+                : [];
+        for (const testcase of testcases) {
+            count++;
+            if (testcase.skipped) skipped++;
+            if (testcase.failure || (testcase.flakyFailure && !ignoreFlakyTests) || testcase.error) {
+                let testcaseData =
+                    (testcase.failure && testcase.failure._cdata) ||
+                    (testcase.failure && testcase.failure._text) ||
+                    (testcase.flakyFailure && testcase.flakyFailure._cdata) ||
+                    (testcase.flakyFailure && testcase.flakyFailure._text) ||
+                    (testcase.error && testcase.error._cdata) ||
+                    (testcase.error && testcase.error._text) ||
+                    '';
+                testcaseData = Array.isArray(testcaseData) ? testcaseData : [testcaseData];
+                const stackTrace = (testcaseData.length ? testcaseData.join('') : '').trim();
+
+                const message = (
+                    (testcase.failure &&
+                        testcase.failure._attributes &&
+                        testcase.failure._attributes.message) ||
+                    (testcase.flakyFailure &&
+                        testcase.flakyFailure._attributes &&
+                        testcase.flakyFailure._attributes.message) ||
+                    (testcase.error &&
+                        testcase.error._attributes &&
+                        testcase.error._attributes.message) ||
+                    stackTrace.split('\n').slice(0, 2).join('\n') ||
+                    testcase._attributes.name
+                ).trim();
+
+                const {filename, filenameWithPackage, line} = resolveFileAndLine(
+                    testcase._attributes.file,
+                    testcase._attributes.classname,
+                    stackTrace,
+                    isFilenameInStackTrace
+                );
+
+                const path = await resolvePath(filenameWithPackage);
+                const title = `${filename}.${testcase._attributes.name}`;
+                core.info(`${path}:${line} | ${message.replace(/\n/g, ' ')}`);
+
+                annotations.push({
+                    path,
+                    start_line: line,
+                    end_line: line,
+                    start_column: 0,
+                    end_column: 0,
+                    annotation_level: 'failure',
+                    title,
+                    message,
+                    raw_details: stackTrace
+                });
+            }
+        }
+    }
+    return {count, skipped, annotations};
+}
+
+const parseTestReports = async (reportPaths, isFilenameInStackTrace, ignoreFlakyTests) => {
+    const globber = await glob.create(reportPaths, {followSymbolicLinks: false});
+    let annotations = [];
+    let count = 0;
+    let skipped = 0;
+    for await (const file of globber.globGenerator()) {
+        const {count: c, skipped: s, annotations: a} = await parseFile(file, isFilenameInStackTrace, ignoreFlakyTests);
+        if (c === 0) continue;
+        count += c;
+        skipped += s;
+        annotations = annotations.concat(a);
+    }
+    return {count, skipped, annotations};
+};
+
+module.exports = { resolveFileAndLine, resolvePath, parseFile, parseTestReports };
